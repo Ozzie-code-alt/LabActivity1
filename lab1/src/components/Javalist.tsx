8677865a45e1f9f7ad1348cbfe8af6329537db3c
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-09-05 23:18:05 +0800

    Added Effects in ANime js

--- lab1/src/components/Javalist.tsx
+++ lab1/src/components/Javalist.tsx
@@ -1,11 +1,42 @@
 import { ListGroup } from "react-bootstrap"
+import { useEffect, useRef } from 'react';
+import anime from 'animejs'
 
 function Javalist(props: {javaValue:string[]}) {
 
+    const javaGroupRef = useRef(null )
     const {javaValue} = props
+
+    useEffect(() => {
+        // Fade in animation configuration
+        if(javaGroupRef.current){
+          anime.timeline({
+           loop: false
+          })
+          
+        .add({
+          targets: javaGroupRef.current.children,
+          opacity: [0, 1],
+          width:'10%',
+        //   duration: 1000, // Animation duration in milliseconds
+          easing: 'easeOutQuad', // Animation easing function
+          direction: 'alternate',
+          loop: true,
+        //   delay: anime.stagger(600), // Delay between animations for each item
+        })
+        
+    
+    
+      }
+    }, [javaValue]);
+
+
+
+
+
   return (
     <div>
-         <ListGroup horizontal>
+         <ListGroup horizontal ref={javaGroupRef}>
             {javaValue.map((javaValuesm, index)=>{
                 return <ListGroup.Item key={index}>{javaValuesm}</ListGroup.Item>
             })}
--- lab1/src/components/PythonList.tsx
+++ lab1/src/components/PythonList.tsx
@@ -1,12 +1,36 @@
 
 import { ListGroup} from 'react-bootstrap'
-
+import {useEffect, useRef} from 'react'
+import anime from 'animejs'
 function PythonList(props: {pythonValue:string[]}) {
-
     const {pythonValue} = props
+    const pythonGroupRef = useRef(null)
+
+    useEffect(() => {
+        // Fade in animation configuration
+        if(pythonGroupRef.current){
+          anime.timeline({
+           loop: false
+          })
+          
+        .add({
+          targets: pythonGroupRef.current.children,
+          opacity: [0, 1],
+          width:'10%',
+        //   duration: 1000, // Animation duration in milliseconds
+          easing: 'easeOutQuad', // Animation easing function
+          direction: 'alternate',
+          loop: true,
+        //   delay: anime.stagger(600), // Delay between animations for each item
+        })
+        
+    
+    
+      }
+    }, [pythonValue]);
   return (
     <div>
-        <ListGroup horizontal>
+        <ListGroup horizontal ref={pythonGroupRef}>
         {pythonValue.map((currPython, currIndex) =>{
             return <ListGroup.Item key={currIndex}>{currPython}</ListGroup.Item>
         })}

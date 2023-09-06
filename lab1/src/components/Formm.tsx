1fc04d890b04f6a0629ce286b6e90223ce390492
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-09-06 23:11:35 +0800

    someUpdates

--- lab1/src/components/Formm.tsx
+++ lab1/src/components/Formm.tsx
@@ -1,146 +1,264 @@
-
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import { Dropdown, InputGroup, OverlayTrigger, Stack, Tooltip } from 'react-bootstrap'
-import { ChangeEvent, useState } from 'react'
-import List from './List'
-import PythonList from './PythonList'
-import Javalist from './Javalist'
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import {
+  Dropdown,
+  InputGroup,
+  OverlayTrigger,
+  Stack,
+  Tooltip,
+} from "react-bootstrap";
+import { ChangeEvent, useState } from "react";
+import List from "./List";
+import PythonList from "./PythonList";
+import Javalist from "./Javalist";
+import Alert from 'react-bootstrap/Alert';
 
 function Formm() {
-
   interface myObject {
-    [key:string]: string
+    [key: string]: string;
   }
-    const [message, setMessage] = useState('')
-    const [splittedHEHE, setSplitted] = useState<string[]>([]);
+  const [message, setMessage] = useState("");
+  const [splittedHEHE, setSplitted] = useState<string[]>([]);
 
+  const [dropdownString, setDropdown] = useState("");
+  const [show, setShowError] = useState(true);
 
-    const [dropdownString, setDropdown] = useState('')
+  const pythonTokensContainer: myObject = {
+    'sum': "Identifier",
+    'let': "KeyWord",
+    "=": "operator",
+    "(": "OpenParenthesis",
+    ")": "ClosedParenthesis",
+    '+': 'Operator',
+    '-': 'Operator',
+    '*':'Operator',
+    '/':'Operator',
+  };
 
-    const objectAnswers: myObject ={
-      "int": "Identifier",
-      "let": "KeyWord",
-      "=": "operator",
-      "100": "integer",
-      '(' : 'OpenParenthesis',
-      ')': 'ClosedParenthesis',
-      'sum': 'identifier',
-      
-    }
-
-
-
-    const handleChange = (event: ChangeEvent<HTMLInputElement>)=>{
-        setMessage(event.target.value)
-        
-    }
-
-    const handleClick = ()=>{
-        const printAnswer = labFunction(message)
-        setSplitted(printAnswer)
-    }
-
-
-    const labFunction = (word:string)=>{
-
-      let modifiedString = ''
-      let valueAdded = ''
-
-        for(let i=0;i<word.length;i++){
-        if(word[i] === "(" || word[i] === ")"){
-          modifiedString += ' ' + word[i] + ' ' 
-        }
-        else{
-            modifiedString += word[i]
-        }
-        // console.log(indexPerenthesis)
-        }
-
-        // console.log(modifiedString)
-        let splitted = modifiedString.split(" ")
-        console.log(splitted)
-
-        splitted.forEach((value) =>{
-
-         if(objectAnswers.hasOwnProperty(value)){
-          valueAdded += ' ' + value + ":" + objectAnswers[value] + ' '
-         }
-         else if(value === "(" || value === ")" || value == "="){
-          valueAdded+= ' ' + value +' '
-         }
-         else{
-          valueAdded += value
-         }
-        })
-        console.log(valueAdded)
-
-        let heheWorks = valueAdded.split(" ")
-        
-        console.log(heheWorks)
-        // console.log(splitted)
-        return heheWorks
-        // let wordSeparated = word.split(" ")
-        // let indexParenthesis = wordSeparated.indexOf("(")
-
-        // console.log(wordSeparated[indexParenthesis])
-        // console.log(wordSeparated)
-        
-    }
-
-
-    const UpdateButton = (btnName:string)=>{
-        console.log(btnName)
-        setDropdown(btnName)
-    }
-
-    const renderToolTip = (HEHEHE:string)=>{
-     return <Tooltip id='tooltip'>{HEHEHE}</Tooltip>
-    }
-
-
-    const PythonTokenss = ['KeyWords', 'identifier', 'Literals', 'Delimiters', ' Operators']
-    const JavaTokenss = ['KeyWords', 'identifier', 'Literals', ' Operators', 'Separator','Comments']
-  return (
-    <div>
-        <Form>
-
-            <Form.Group className='mb-3' controlId='form1'  >
-
-              <Stack gap={3}>
-            <Form.Label>Input Text Here</Form.Label>
-            <Form.Label > { dropdownString  == 'Python' ? <PythonList pythonValue = {PythonTokenss}/> : dropdownString == 'Java' ? <Javalist javaValue = {JavaTokenss}/> : null }</Form.Label>
-            </Stack>
-
-                <InputGroup>
-              <Dropdown>
-                <Dropdown.Toggle variant='success' > 
-                  {dropdownString ? dropdownString : 'Choose a Language'}
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                  <OverlayTrigger placement='right' overlay={renderToolTip('Python Tokens: \n Keywords \n identifier \n Literals \n Delimiters \n Operators')}>
-                  <Dropdown.Item  onClick={() => UpdateButton('Python')}>Python</Dropdown.Item>
-                  </OverlayTrigger>
-                  
-                  <OverlayTrigger placement='right' overlay={renderToolTip('Java Tokens:  \n Keywords \n identifier \n Literals \n Operators \n Separators \n Comments')}>
-                  <Dropdown.Item onClick={()=> UpdateButton('Java')}>Java</Dropdown.Item>
-                  </OverlayTrigger>
-        
-                </Dropdown.Menu>
-              </Dropdown>
-            <Form.Control type='text' value={message} onChange={handleChange} placeholder='Place assignment query here'/>
-            <Button variant='outline-secondary' onClick={handleClick}>Check it Out</Button>
-            </InputGroup>
-            </Form.Group>
-        </Form>
-        <List splittedData = {splittedHEHE}/>
-        </div>
-  )
-  
-
-
+const javaTokensContainer: myObject= {
+"out" : 'Identifier',
+"println" : 'Identifier',
+"main": 'Identifier',
+"args":"Identifier",
+"String" : "Identifier",
+'class':'Keywords',
+'public':'Keywords',
+'static':'Keywords',
+'if':'Keywords',
+'void' : 'Keywords',
+'while':'Keywords',
+'return' :'Keywords',
+'(':  'Separators',
+')':  'Separators',
+'{': 'Separators',
+'}': 'Separators',
+';': 'Separators',
+',':'Separators',
+'"' : 'Separators',
+"." : 'Separators',
+"=" : "Operator",
+"[" : "Separator",
+"]" : "Separator"
 }
 
-export default Formm
+
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+
+  const handleClick = () => {
+    console.log(dropdownString);
+    const printAnswer = labFunction(message);
+    setSplitted(printAnswer);
+  };
+
+
+
+  const labFunction = (word: string) => {
+    let modifiedString = "";
+    let valueAdded = "";
+
+    for (let i = 0; i < word.length; i++) {
+    
+      if (word[i] === "(" || word[i] === ")" || word[i]  === "." || word[i] === "," || word[i]=== ";"|| word[i]==="[" || word[i] ==="]") {
+        modifiedString += " " + word[i] + " ";
+      } else {
+        modifiedString += word[i]  ;
+      }
+      // console.log(indexPerenthesis)
+    }
+
+    // console.log(modifiedString)
+    let splitted = modifiedString.split(" ").filter(filtering => filtering !=='');
+    console.log(splitted);
+
+    if (dropdownString === "Python") {
+      splitted.forEach((value) => {
+        if (pythonTokensContainer.hasOwnProperty(value)) {
+          valueAdded += " " + value + ":" + pythonTokensContainer[value] + " ";
+        } else if (value === "(" || value === ")" || value == "=") {
+          valueAdded += " " + value + " ";
+        } else {
+          valueAdded += value;
+        }
+      });
+      console.log(valueAdded);
+
+      let heheWorks = valueAdded.split(" ");
+
+      console.log(heheWorks);
+      // console.log(splitted)
+      return heheWorks;
+      // let wordSeparated = word.split(" ")
+      // let indexParenthesis = wordSeparated.indexOf("(")
+
+      // console.log(wordSeparated[indexParenthesis])
+      // console.log(wordSeparated)
+    } else if (dropdownString === "Java") {
+          const numberStrings = []
+
+      splitted.forEach((value) => {
+        const potentialNumber = parseInt(value, 10)
+        if(!isNaN(potentialNumber)){
+          numberStrings.push(value)
+        }
+          
+        if (javaTokensContainer.hasOwnProperty(value)) {
+            valueAdded += " " + value + ":" + javaTokensContainer[value] + " ";
+          
+        } else if (value === "(" || value === ")" || value == "=") {
+            valueAdded += " " + value + " ";  
+        }
+      else {
+          valueAdded += value + ":Literal ";
+        }
+      });
+      // console.log(valueAdded);
+
+      let heheWorks = valueAdded.split(" ")
+     
+
+      console.log(heheWorks);
+      // console.log(splitted)
+      return heheWorks;
+      // let wordSeparated = word.split(" ")
+      // let indexParenthesis = wordSeparated.indexOf("(")
+
+      // console.log(wordSeparated[indexParenthesis])
+      // console.log(wordSeparated)
+
+    }
+    else{
+      console.log("Workjss ?")
+       setShowError(true)
+    }
+  };
+
+  const UpdateButton = (btnName: string) => {
+    setDropdown(btnName);
+  };
+
+  const renderToolTip = (HEHEHE: string) => {
+    return <Tooltip id="tooltip">{HEHEHE}</Tooltip>;
+  };
+
+  
+   
+  
+
+  const PythonTokenss = [
+    "KeyWords",
+    "identifier",
+    "Literals",
+    "Delimiters",
+    " Operators",
+  ];
+  const JavaTokenss = [
+    "KeyWords",
+    "identifier",
+    "Literals",
+    " Operators",
+    "Separator",
+    "Comments",
+  ];
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="form1">
+          <Stack gap={3}>
+            <Form.Label>Input Text Here</Form.Label>
+            <Form.Label>
+              {" "}
+              {dropdownString == "Python" ? (
+                <PythonList pythonValue={PythonTokenss} />
+              ) : dropdownString == "Java" ? (
+                <Javalist javaValue={JavaTokenss} />
+              ) : null}
+            </Form.Label>
+          </Stack>
+
+
+          <Alert show={show} variant="success">
+        <Alert.Heading>My Alert</Alert.Heading>
+        <p>
+    Please dont forget to choose a Language otherwiwse it will return an error
+
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShowError(false)} variant="outline-success">
+            Close me
+          </Button>
+        </div>
+      </Alert>
+
+
+          <InputGroup>
+            <Dropdown>
+              <Dropdown.Toggle variant="success">
+                {dropdownString ? dropdownString : "Choose a Language"}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <OverlayTrigger
+                  placement="right"
+                  overlay={renderToolTip(
+                    "Python Tokens: \n Keywords \n identifier \n Literals \n Delimiters \n Operators"
+                  )}
+                >
+                  <Dropdown.Item onClick={() => UpdateButton("Python")}>
+                    Python
+                  </Dropdown.Item>
+                </OverlayTrigger>
+
+                <OverlayTrigger
+                  placement="right"
+                  overlay={renderToolTip(
+                    "Java Tokens:  \n Keywords \n identifier \n Literals \n Operators \n Separators \n Comments"
+                  )}
+                >
+                  <Dropdown.Item onClick={() => UpdateButton("Java")}>
+                    Java
+                  </Dropdown.Item>
+                </OverlayTrigger>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Form.Control
+              type="text"
+              value={message}
+              onChange={handleChange}
+              placeholder="Place assignment query here"
+            />
+            <Button variant="outline-secondary" onClick={handleClick}>
+              Check it Out
+            </Button>
+          </InputGroup>
+        </Form.Group>
+      </Form>
+      <List splittedData={splittedHEHE} />
+    </div>
+  );
+}
+
+export default Formm;
